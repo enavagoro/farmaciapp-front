ed69a6461cc6f2cd3d23634906b8b88e2c0aa238
Elías Navarrete <e.navagoro@gmail.com>
2020-07-19 11:55:31 -0400

    se agrego selector de cantidades, y la vista de detalle

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController ,ToastController, AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -18,20 +19,28 @@ export class InicioPage implements OnInit {
     }
 
   */
+
+  // (para después de llamar el servicio) -> producto = {id:0,titulo:'',precio:0,descripcion:'',cantidadMaxima:0,estado:false,codigo:''};
+
   banderaBarra = true;
-  banderaPrincipal = false;
+  banderaPrincipal = true;
   productos = [];
+  producto = {"titulo":"","precio":0,"descripcion":"","cantidadMaxima":0,"cantidad":0,"estado":"","codigo":""};
   /* hardcodeo del weno */
 
-  arregloLlegada = [{"titulo":"peineta","precio":"1000","descripcion":"peinetas baratas","cantidadMaxima":"23","estado":"true","codigo":"A-1"},
-                    {"titulo":"pastillas","precio":"3000","descripcion":"pastillas buenas","cantidadMaxima":"15","estado":"true","codigo":"B-2"},
-                    {"titulo":"manzanas","precio":"2000","descripcion":"manzanas baratas","cantidadMaxima":"10","estado":"true","codigo":"C-3"},
-                    {"titulo":"morral","precio":"1500","descripcion":"morral de lana","cantidadMaxima":"9","estado":"true","codigo":"D-4"}
+  arregloLlegada = [{"titulo":"Peineta","precio":1000,"descripcion":"peinetas baratas","cantidadMaxima":23,"estado":"true","codigo":"A-1"},
+                    {"titulo":"Pastillas","precio":3000,"descripcion":"pastillas buenas","cantidadMaxima":15,"estado":"true","codigo":"B-2"},
+                    {"titulo":"Manzanas","precio":2000,"descripcion":"manzanas baratas","cantidadMaxima":10,"estado":"true","codigo":"C-3"},
+                    {"titulo":"Morral","precio":1500,"descripcion":"morral de lana","cantidadMaxima":9,"estado":"true","codigo":"D-4"}
                     ];
 
   productosAgregados = [];
 
-  constructor() {
+  selector = {numeroMinimo: 999, numeroActual: 999, numeroMaximo: 990};
+
+  constructor(private toastController : ToastController,
+              private alertController :AlertController,
+              private modalCtrl : ModalController) {
 
   }
 
@@ -43,7 +52,6 @@ export class InicioPage implements OnInit {
   }
 
   cambiarEstado(valor){
-
     if(valor==0){
       this.banderaPrincipal = !this.banderaPrincipal;
     }
@@ -52,10 +60,80 @@ export class InicioPage implements OnInit {
     }
   }
 
+  abrirDetalle(producto){
+    this.producto = producto;
+    this.cambiarEstado(0);
+    this.iniciarSelector();
+  }
+
+  iniciarSelector(){
+    this.selector.numeroMaximo = this.producto.cantidadMaxima;
+    this.selector.numeroActual = 0;
+    this.selector.numeroMinimo = 0;
+  }
+
+
+  limpiarSelector(){
+    this.selector = {numeroMinimo: 999, numeroActual: 999, numeroMaximo: 990};
+  }
+
+  sumarProducto(){
+    console.log('entré');
+
+    if(this.selector.numeroActual < this.selector.numeroMaximo)
+    {
+      console.log('pasé el if');
+      this.selector.numeroActual ++;
+    }
+
+  }
+
+  restarProducto(){
+    console.log('entré');
+    console.log('numeroActual',this.selector.numeroActual);
+    console.log('numeroMinimo',this.selector.numeroMinimo)
+    if(this.selector.numeroActual > this.selector.numeroMinimo)
+    {
+      console.log('pasé el if');
+      this.selector.numeroActual --;
+    }
+  }
+
+  cerrarDetalle(){
+    this.producto = {"titulo":"","precio":0,"descripcion":"","cantidadMaxima":0,"cantidad":0,"estado":"","codigo":""};
+    this.cambiarEstado(0);
+    this.limpiarSelector();
+  }
+
 /* las funciones aún no están realizadas es mero testeo */
 
-  agregarProductos(producto){
-    this.productosAgregados.push(producto);
-    console.log(this.productosAgregados);
+  agregarProducto(){
+    console.log('holo');
+  }
+
+
+  async confirmarAgregar(){
+
+    const alert = await this.alertController.create({
+      header: '¿Estás Seguro?',
+      message: '¿Deseas agregar este producto al detalle? ',//definir bien el concepto de boleta,detalle,carrito? tu conoces el modelo de negocio :p
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            //console.log('Cancelado');
+          }
+        }, {
+          text: 'Aceptar',
+          handler: () => {
+            this.agregarProducto();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
